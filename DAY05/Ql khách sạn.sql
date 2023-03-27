82cef7ddd7a1f9d8cb031cc734900b40afbb5297
ductoan196 <ductoannd96@gmail.com>
2023-03-28 00:55:25 +0700

    update

---
+++ DAY05/Ql khách sạn.sql
@@ -0,0 +1,110 @@
+CREATE TABLE `rooms` (
+  `id` int(11) PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(255),
+  `location_id` int(11),
+  `description` text,
+  `price` decimal,
+  `capacity` int(11),
+  `status` varchar(255)
+);
+
+CREATE TABLE `users` (
+  `id` int(11) PRIMARY KEY AUTO_INCREMENT,
+  `username` varchar(255),
+  `email` varchar(255),
+  `password` varchar(255),
+  `status` varchar(255)
+);
+
+CREATE TABLE `bookings` (
+  `id` int(11) PRIMARY KEY AUTO_INCREMENT,
+  `user_id` int(11),
+  `total_price` decimal,
+  `status` varchar(255),
+  `check_in` date,
+  `check_out` date
+);
+
+CREATE TABLE `bookings_detail` (
+  `id` int(11) PRIMARY KEY AUTO_INCREMENT,
+  `booking_id` int(11),
+  `room_id` int(11),
+  `menu_id` int(11),
+  `total_price` decimal,
+  `creat_at` date
+);
+
+CREATE TABLE `locations` (
+  `id` int(11) PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(255),
+  `address` varchar(255),
+  `phone` varchar(255),
+  `status` varchar(255)
+);
+
+CREATE TABLE `services` (
+  `id` int(11) PRIMARY KEY AUTO_INCREMENT,
+  `menu_id` int(11),
+  `room_id` int(11),
+  `news_id` int(11)
+);
+
+CREATE TABLE `menus` (
+  `id` int(11) PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(255),
+  `description` text,
+  `price` decimal
+);
+
+CREATE TABLE `vouchers` (
+  `id` int(11) PRIMARY KEY AUTO_INCREMENT,
+  `code` varchar(255),
+  `room_id` int(11),
+  `menu_id` int(11),
+  `desctiption` text,
+  `discount` int(11),
+  `status` varchar(255),
+  `start_at` DATE,
+  `end_at` DATE
+);
+
+CREATE TABLE `news` (
+  `id` int(11) PRIMARY KEY AUTO_INCREMENT,
+  `title` text,
+  `desctiption` text,
+  `post_at` date
+);
+
+CREATE TABLE `pricing` (
+  `id` int(11) PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(255),
+  `price` decimal,
+  `room_id` int(11),
+  `menu_id` int(11)
+);
+
+ALTER TABLE `rooms` ADD FOREIGN KEY (`id`) REFERENCES `bookings_detail` (`room_id`);
+
+ALTER TABLE `menus` ADD FOREIGN KEY (`id`) REFERENCES `bookings_detail` (`menu_id`);
+
+ALTER TABLE `rooms` ADD FOREIGN KEY (`location_id`) REFERENCES `locations` (`id`);
+
+ALTER TABLE `services` ADD FOREIGN KEY (`menu_id`) REFERENCES `menus` (`id`);
+
+ALTER TABLE `services` ADD FOREIGN KEY (`room_id`) REFERENCES `rooms` (`id`);
+
+ALTER TABLE `services` ADD FOREIGN KEY (`news_id`) REFERENCES `news` (`id`);
+
+ALTER TABLE `bookings` ADD FOREIGN KEY (`id`) REFERENCES `bookings_detail` (`booking_id`);
+
+ALTER TABLE `bookings` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+ALTER TABLE `vouchers` ADD FOREIGN KEY (`room_id`) REFERENCES `rooms` (`id`);
+
+ALTER TABLE `vouchers` ADD FOREIGN KEY (`menu_id`) REFERENCES `menus` (`id`);
+
+ALTER TABLE `pricing` ADD FOREIGN KEY (`menu_id`) REFERENCES `menus` (`id`);
+
+ALTER TABLE `pricing` ADD FOREIGN KEY (`room_id`) REFERENCES `rooms` (`id`);
+
+ALTER TABLE `bookings` ADD FOREIGN KEY (`total_price`) REFERENCES `bookings_detail` (`total_price`);
