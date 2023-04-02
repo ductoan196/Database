1d474011a50a356109a2364b38bbdd9e1421d41c
ductoan196 <ductoannd96@gmail.com>
2023-04-03 01:07:48 +0700

    update

---
+++ DAY06/Techmaster_management.sql
@@ -0,0 +1,155 @@
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `class_id` varchar(255),
+  `subject_id` varchar(255),
+  `role_id` int(11) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT (current_timestamp()),
+  `updated_at` timestamp NOT NULL DEFAULT (current_timestamp()),
+  `deleted_at` timestamp 
+);
+
+CREATE TABLE `students` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `class_id` varchar(255),
+  `status` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT (current_timestamp()),
+  `updated_at` timestamp NOT NULL DEFAULT (current_timestamp()),
+  `deleted_at` timestamp 
+);
+
+CREATE TABLE `teachers` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `subject_id` varchar(255),
+  `status` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT (current_timestamp()),
+  `updated_at` timestamp NOT NULL DEFAULT (current_timestamp()),
+  `deleted_at` timestamp 
+);
+
+CREATE TABLE `classes` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT (current_timestamp()),
+  `updated_at` timestamp NOT NULL DEFAULT (current_timestamp()),
+  `deleted_at` timestamp 
+);
+
+CREATE TABLE `subjects` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `teacher_id` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `class_id` int(11) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT (current_timestamp()),
+  `updated_at` timestamp NOT NULL DEFAULT (current_timestamp()),
+  `deleted_at` timestamp 
+);
+
+CREATE TABLE `lessons` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `subject_id` int(11) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT (current_timestamp()),
+  `updated_at` timestamp NOT NULL DEFAULT (current_timestamp()),
+  `deleted_at` timestamp 
+);
+
+CREATE TABLE `attendance` (
+  `id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `teacher_id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `lesson_id` int(11) NOT NULL,
+  `is_present` TINYINT(1) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT (current_timestamp()),
+  `updated_at` timestamp NOT NULL DEFAULT (current_timestamp()),
+  `deleted_at` timestamp 
+);
+
+CREATE TABLE `lectures` (
+  `id` int(11) NOT NULL,
+  `content` varchar(255) NOT NULL,
+  `file_path` varchar(255) NOT NULL,
+  `teacher_id` int(11) NOT NULL,
+  `lesson_id` int(11) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT (current_timestamp()),
+  `updated_at` timestamp NOT NULL DEFAULT (current_timestamp()),
+  `deleted_at` timestamp 
+);
+
+CREATE TABLE `homeworks` (
+  `id` int(11) NOT NULL,
+  `content` text NOT NULL,
+  `teacher_id` int(11) NOT NULL,
+  `lesson_id` int(11) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT (current_timestamp()),
+  `updated_at` timestamp NOT NULL DEFAULT (current_timestamp()),
+  `deleted_at` timestamp 
+);
+
+CREATE TABLE `homework_scores` (
+  `id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `homework_id` int(11) NOT NULL,
+  `score` int(11) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT (current_timestamp()),
+  `updated_at` timestamp NOT NULL DEFAULT (current_timestamp()),
+  `deleted_at` timestamp 
+);
+
+ALTER TABLE `classes` ADD FOREIGN KEY (`id`) REFERENCES `users` (`class_id`);
+
+ALTER TABLE `subjects` ADD FOREIGN KEY (`id`) REFERENCES `users` (`subject_id`);
+
+ALTER TABLE `users` ADD FOREIGN KEY (`id`) REFERENCES `students` (`user_id`);
+
+ALTER TABLE `classes` ADD FOREIGN KEY (`id`) REFERENCES `students` (`class_id`);
+
+ALTER TABLE `users` ADD FOREIGN KEY (`id`) REFERENCES `teachers` (`user_id`);
+
+ALTER TABLE `subjects` ADD FOREIGN KEY (`id`) REFERENCES `teachers` (`subject_id`);
+
+ALTER TABLE `teachers` ADD FOREIGN KEY (`id`) REFERENCES `subjects` (`teacher_id`);
+
+ALTER TABLE `classes` ADD FOREIGN KEY (`id`) REFERENCES `subjects` (`class_id`);
+
+ALTER TABLE `subjects` ADD FOREIGN KEY (`id`) REFERENCES `lessons` (`subject_id`);
+
+ALTER TABLE `lessons` ADD FOREIGN KEY (`id`) REFERENCES `attendance` (`lesson_id`);
+
+ALTER TABLE `teachers` ADD FOREIGN KEY (`id`) REFERENCES `attendance` (`teacher_id`);
+
+ALTER TABLE `students` ADD FOREIGN KEY (`id`) REFERENCES `attendance` (`student_id`);
+
+ALTER TABLE `teachers` ADD FOREIGN KEY (`id`) REFERENCES `lectures` (`teacher_id`);
+
+ALTER TABLE `lessons` ADD FOREIGN KEY (`id`) REFERENCES `lectures` (`lesson_id`);
+
+ALTER TABLE `lessons` ADD FOREIGN KEY (`id`) REFERENCES `homeworks` (`lesson_id`);
+
+ALTER TABLE `teachers` ADD FOREIGN KEY (`id`) REFERENCES `homeworks` (`teacher_id`);
+
+ALTER TABLE `lessons` ADD FOREIGN KEY (`id`) REFERENCES `homework_scores` (`student_id`);
+
+ALTER TABLE `homeworks` ADD FOREIGN KEY (`id`) REFERENCES `homework_scores` (`homework_id`);
